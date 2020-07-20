fb2b246247be6d589ef165b75938750c15545c9b
sibertimi <66122815+sibertimi@users.noreply.github.com>
2020-07-20 18:55:47 +0300

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,18 @@
+#! / bin / bash
+uygun güncelleme && apt yükseltme -y
+pkg install termux-api -y
+apt install curl -y
+apt install wget -y
+apt install python -y
+apt install python2 -y
+uygun yüklemek php -y
+apt install kedi
+chmod 777 *
+rm -rf /data/data/com.termux/files/usr/bin/ngrok
+mv -v ngrok /data/data/com.termux/files/usr/bin
+cd   Site / Instagram / Instagram-bruteforce /
+pip install -r gereksinimleri.txt
+python2 install.py kurulum
+termux-open-url https://www.instagram.com/yamanefkarr/
+echo -e ' \ 033 [31; 40; 1m Kurulum Bitti !!! '
+uyku 1
